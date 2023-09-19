7a1dd1e3ba3cac566896ef3e1ad34a0fcb005b4c
ashenhair <100282383+ashenhair@users.noreply.github.com>
2023-09-19 22:05:54 +0300

    Update Slider.tsx
    slider is updated

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Slider = () => {
   return (
-    <div>
-      Slider
+    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
+        {/* TEXT CONTAINER */}
+        <div className='h-1/2flex items-center justify-center flex-col gap-8 text-red-800 font-bold'>
+        <h1 className='text-5xl text-center uppercase p-4 md:text-6xl xl:text-7xl'>
+            Test
+        </h1>
+        <button className='bg-red-800 text-white py-4 px-8'>Order Now</button>
+        </div>
+        
+        <div className='w-full h-1/2 relative'>
+            <Image src="/slide1.png" alt="" fill className='object-cover'/>
+        </div>
+
     </div>
   )
 }
