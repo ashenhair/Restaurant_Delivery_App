540ac2c8a412f6efc8b5e827fbe1504512d41f88
ashenhair <100282383+ashenhair@users.noreply.github.com>
2023-10-10 13:41:17 +0300

    Update Featured.tsx

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -2,10 +2,27 @@ import React from 'react'
 
 const Featured = () => {
   return (
-    <div>
-      Featured
+    <div className=''>
+      {/* WRAPPER} */}
+      <div className=''>
+        {/* SINGLE ITEM */}
+        <div className=''>
+          {/* IMAGE CONTAINER */}
+          <div className='relative'>
+            {/* IMAGE */}
+          </div>
+          {/* TEXT CONTAINER */}
+          <div className=''>
+            <h1 className=''>Title</h1>
+            <p>Desc</p>
+            <span>123</span>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+)
 }
 
 export default Featured
+
