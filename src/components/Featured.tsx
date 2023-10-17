ca971dea97767aa8c911bd3b91628762325c5358
ashenhair <100282383+ashenhair@users.noreply.github.com>
2023-10-17 15:06:50 +0300

    Update Featured.tsx

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,22 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Featured = () => {
   return (
-    <div className=''>
+    <div className='w-screen overflow-x-scroll text-red-500'>
       {/* WRAPPER} */}
-      <div className=''>
+      <div className='w-max flex'>
         {/* SINGLE ITEM */}
-        <div className=''>
+        <div className='w-screen h-[60vh] flex flex-col items-center justify-around p-4'>
           {/* IMAGE CONTAINER */}
-          <div className='relative'>
-            {/* IMAGE */}
+          <div className='relative flex-1 w-full'>
+            <Image src="/p1.png" alt="" fill className='object-contain' />
           </div>
           {/* TEXT CONTAINER */}
-          <div className=''>
-            <h1 className=''>Title</h1>
+          <div className='flex-1 flex flex-col gap-4'>
+            <h1 className='text-xl font-bold uppercase'>Title</h1>
             <p>Desc</p>
-            <span>123</span>
-            <button>Add to Cart</button>
+            <span className='text-xl font-bold'>123</span>
+            <button className='bg-red-500 text-white p-2 rounded-md'>Add to Cart</button>
           </div>
         </div>
       </div>
@@ -25,4 +26,5 @@ const Featured = () => {
 }
 
 export default Featured
+
 
