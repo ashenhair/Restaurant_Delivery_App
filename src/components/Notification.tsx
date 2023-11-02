1a7cf31afc7161cf777e887611283d161040faaa
ashenhair <100282383+ashenhair@users.noreply.github.com>
2023-11-02 20:50:08 +0300

    Update Notification.tsx

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const Notification = () => {
   return (
     <div className='h-12 bg-red-800 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>
